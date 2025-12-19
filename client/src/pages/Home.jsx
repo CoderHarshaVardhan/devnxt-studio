713685b95d4b34b0f91ad2d7fcf0a72d9377eef7
Harsha <137918319+CoderHarshaVardhan@users.noreply.github.com>
2025-12-19 11:46:56 +0530

    Updated home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -719,8 +719,8 @@ function Home() {
             
             <div className="footer-section footer-contact">
               <h4>Get in Touch</h4>
-              <p><Mail size={16} className="footer-icon" /> hello@devnxtstudio.com</p>
-              <p><Phone size={16} className="footer-icon" /> +91 1234567890</p>
+              <p><Mail size={16} className="footer-icon" /> narojuharsha2004@gmail.com</p>
+              <p><Phone size={16} className="footer-icon" /> +91 8179808340</p>
               <p><MapPin size={16} className="footer-icon" /> Hyderabad, Telangana</p>
             </div>
           </div>
